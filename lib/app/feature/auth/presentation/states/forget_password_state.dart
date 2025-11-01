32f68f9716f1eef0adb02ff0ce7fcb9e33b9fb75
Hager Darwish <107562928+Hagerdarwish@users.noreply.github.com>
2025-11-01 23:24:55 +0200

    add cubit

---
+++ lib/app/feature/auth/presentation/states/forget_password_state.dart
@@ -0,0 +1,6 @@
+import 'package:exam_app/app/config/base_state/base_state.dart';
+import 'package:exam_app/app/feature/auth/data/models/forget_password_email_response_model.dart';
+
+class ForgetPasswordState extends BaseState<ForgetPasswordEmailResponseModel>{
+
+}
--- lib/app/feature/auth/presentation/viewmodel/forget_password_view_model.dart
+++ lib/app/feature/auth/presentation/viewmodel/forget_password_view_model.dart
@@ -1,12 +1,15 @@
 import 'package:exam_app/app/feature/auth/data/models/forget_password_email_response_model.dart';
+import 'package:exam_app/app/feature/auth/presentation/states/forget_password_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/usecase/send_forget_password_email_usecase.dart';
 
-class ForgetPasswordViewmodel {
-  ForgetPasswordViewmodel(this.sendForgetPasswordEmailUseCase);
+class ForgetPasswordViewmodel extends Cubit<ForgetPasswordState> {
+  ForgetPasswordViewmodel(this.sendForgetPasswordEmailUseCase):super(ForgetPasswordState());
   final SendForgetPasswordEmailUseCase sendForgetPasswordEmailUseCase;
   void sendForgetPassword(String email) async {
     final response = await sendForgetPasswordEmailUseCase(email: email);
+
     print(response.toString());
   }
 }
